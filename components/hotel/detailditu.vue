21914c45ad2d8bb5181868c016331b20fefea2d7
ZHJ <1803372589@qq.com>
2019-08-09 09:32:40 +0800

    酒店详情页面基本完成，准备获取数据

---
+++ components/hotel/detailditu.vue
@@ -0,0 +1,171 @@
+<template>
+  <!-- 地图部分 -->
+  <el-row class="map" type="flex">
+    <div class="map-l">
+<fafafa/>
+    </div>
+
+    <div class="map-r">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="风景" name="first">
+          <ol>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+            <li>
+              <span>高佬庄</span>
+              <span>13.5公里</span>
+            </li>
+          </ol>
+        </el-tab-pane>
+        <el-tab-pane label="交通" name="second">
+          <ol>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+            <li>
+              <span>甘霖路口(公交站)</span>
+              <span>5.5公里</span>
+            </li>
+          </ol>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </el-row>
+</template>
+<script>
+import fafafa from '@/components/hotel/ditu/ditu.vue'
+export default {
+  components:{
+    fafafa
+  },
+  data() {
+    return {
+      activeName: "first"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.map {
+  height: 360px;
+  .map-l {
+    margin-right: 20px;
+    width: 650px;
+    height: 100%;
+    // border: 1px solid #666;
+  }
+  .map-r {
+    width: 330px;
+    height: 100%;
+    border: 1px solid #999;
+
+    ol {
+      overflow: auto;
+      height: 300px;
+      li {
+        display: flex;
+        margin: 15px 0;
+        &:nth-child(1) {
+          margin-top: 0;
+        }
+        span {
+          font-size: 14px;
+          color: #666;
+          &:nth-child(1) {
+            padding-left: 10px;
+            width: 230px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ components/hotel/detailprice.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- 酒店价格 -->
+    
+    <el-table :data="tableData2" style="width: 100%">
+      <el-table-column prop="products.name" label="价格来源" width="420"></el-table-column>
+      <el-table-column prop="name" label="低价房型" width="420"></el-table-column>
+      <el-table-column prop="address" label="最低价/每晚"></el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+export default {
+  // 接收父组件传来的数据
+  props: {
+    products: {
+      type: Array,
+      default:[ ]
+    }
+  },
+
+  mounted(){
+    setTimeout(()=>{
+ console.log(this.products)
+    },1000)
+    
+  },
+
+  // 酒店价格
+  methods: {
+
+
+
+    tableRowClassName({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return "warning-row";
+      } else if (rowIndex === 3) {
+        return "success-row";
+      }
+      return "";
+    }
+  },
+  data() {
+    return {
+      tableData2: [
+        {
+          date: "携程",
+          name: "高级大床房",
+          address: "￥150起"
+        },
+        {
+          date: "艺龙",
+          name: "高级大床房",
+          address: "￥600起"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
